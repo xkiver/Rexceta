fdac5b7db28902dbc87c7b522bf1e6677dc29e06
nvalenzu <nicolas.valenzuelam@alumnos.usm.cl>
2016-09-25 13:40:53 -0300

    se agrega funcionalidad de expandir a la barra de busqueda y escribir en ella

--- rexceta_proyect/app/src/main/java/com/multimedios/proyecto/rexceta_proyect/ActivityAB.java
+++ rexceta_proyect/app/src/main/java/com/multimedios/proyecto/rexceta_proyect/ActivityAB.java
@@ -3,14 +3,19 @@ package com.multimedios.proyecto.rexceta_proyect;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.MenuItemCompat.OnActionExpandListener;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class ActivityAB extends AppCompatActivity {
+
+public class ActivityAB extends AppCompatActivity implements OnQueryTextListener, OnActionExpandListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,15 @@ public class ActivityAB extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.menu_activity_ab, menu);
-        return true;
+
+        MenuItem searchItem = menu.findItem(R.id.menu_buscar);
+
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        MenuItemCompat.setOnActionExpandListener(searchItem, this);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -59,11 +72,35 @@ public class ActivityAB extends AppCompatActivity {
             toast1.show();
             return true;
         }
-        else if(id==R.id.opcion4)
+        else if(id==R.id.menu_buscar)
         {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuItemActionExpand(MenuItem item) {
+        Toast.makeText(getApplicationContext(), "Busqueda de receta", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemActionCollapse(MenuItem item) {
+        Toast.makeText(getApplicationContext(), "COLLAPSE", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        //texto.setText("Texto a buscar\n\n" + s);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        //texto.setText("Escribiendo texto...\n\n" + s);
+        return true;
     }
 }
