d7fcd0d7cae7a2f1325d95558fbecd772d56109e
JesiSalazar <jesi.salazar.n@gmail.com>
2016-10-04 23:13:03 -0300

    primer commit jesi

---
+++ app/src/main/java/com/example/jesi/rexceta/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.jesi.rexceta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int MILISEGUNDOS_ESPERA = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "Bienvenido a Rexceta", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        }, MILISEGUNDOS_ESPERA);
+
+    }
+}
+
